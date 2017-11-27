06f1aa05626871f8b84024cb9f00162a21eab15a
iliberova <iva.liberova@gmail.com>
2017-11-27 21:18:49 +0100

    AlbumDao - string pattern search, released albums between dates

--- music-library-persistence/src/main/java/cz/fi/muni/pa165/musiclibrary/dao/AlbumDaoImpl.java
+++ music-library-persistence/src/main/java/cz/fi/muni/pa165/musiclibrary/dao/AlbumDaoImpl.java
@@ -9,7 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,29 +28,29 @@ public class AlbumDaoImpl implements AlbumDao {
 	private EntityManager em;
 
 	@Override
-	public void create(Album album) {
+	public void create(Album album){
 		em.persist(album);
 	}
 
 	@Override
-	public void update(Album album) {
+	public void update(Album album){
 		em.merge(album);
 	}
 
 	@Override
-	public void remove(Album album) {
+	public void remove(Album album){
 		em.remove(findById(album.getId()));
 	}
 
 	@Override
-	public Album findById(Long id) {
+	public Album findById(Long id){
 		return em.find(Album.class, id);
 	}
 
 	@Override
-	public List<Album> findByMusician(Musician musician) {
+	public List<Album> findByMusician(Musician musician){
 		List<Song> songs = em.createQuery("SELECT s FROM Song s WHERE s.musician = :musician",
-			Song.class).setParameter("musician", musician).getResultList();
+				Song.class).setParameter("musician", musician).getResultList();
 		List<Album> albums = new ArrayList<>();
 		for (Song song : songs) {
 			albums.add(song.getAlbum());
@@ -57,9 +59,9 @@ public class AlbumDaoImpl implements AlbumDao {
 	}
 
 	@Override
-	public List<Album> findByGenre(Genre genre) {
+	public List<Album> findByGenre(Genre genre){
 		List<Song> songs = em.createQuery("SELECT s FROM Song s WHERE s.genre = :genre",
-			Song.class).setParameter("genre", genre).getResultList();
+				Song.class).setParameter("genre", genre).getResultList();
 		List<Album> albums = new ArrayList<>();
 		for (Song song : songs) {
 			albums.add(song.getAlbum());
@@ -68,13 +70,34 @@ public class AlbumDaoImpl implements AlbumDao {
 	}
 
 	@Override
-	public List<Album> findByTitle(String titlePattern) {
-		return em.createQuery("SELECT a FROM Album a WHERE a.title like :title ",
-			Album.class).setParameter("title", "%" + titlePattern + "%").getResultList();
+	public List<Album> findByTitle(List<String> titlePatterns){
+		StringBuilder queryBuilder = new StringBuilder("SELECT a FROM Album a");
+
+		for (int i = 0; i < titlePatterns.size(); i++) {
+			if (i == 0) {
+				queryBuilder.append(" WHERE");
+			} else {
+				queryBuilder.append(" AND");
+			}
+			queryBuilder.append(" a.title LIKE :pattern").append(i);
+		}
+
+		TypedQuery<Album> query = em.createQuery(queryBuilder.toString(), Album.class);
+
+		for (int i = 0; i < titlePatterns.size(); i++) {
+			query.setParameter("pattern" + i, titlePatterns.get(i));
+		}
+		return query.getResultList();
 	}
 
 	@Override
 	public List<Album> findAll() {
 		return em.createQuery("SELECT a FROM Album a", Album.class).getResultList();
+	}
+
+	@Override
+	public List<Album> getAlbumsReleasedBetween(Date startDate, Date endDate) {
+		return em.createQuery("SELECT a FROM Album a WHERE a.releaseDate BETWEEN :startDate AND :endDate",
+				Album.class).setParameter("startDate", startDate).setParameter("endDate", endDate).getResultList();
 	}
 }
